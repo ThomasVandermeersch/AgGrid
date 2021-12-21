24bc088c4576290df950abc7ba10a42dffc0a069
ThomasVandermeersch <17030@ecam.be>
2021-12-21 12:00:50 +0100

    First steps with AG Grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { ColDef } from 'ag-grid-community';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ag-grid';
+
+    defaultColDef: ColDef = {
+        sortable: true,
+        filter: true
+    };
+
+    
+
+    columnDefs: ColDef[] = [
+        {field: 'name'},
+        {field: 'solution',rowGroup:true},
+        {field: 'createdAt',
+            cellRenderer: (data) => {
+                return data.value ? (new Date(data.value)).toLocaleDateString() : '';
+            },
+            sortable:true
+        },
+        {field: 'status', rowGroup:true},
+        {field: 'maxDevices', filter: 'agNumberColumnFilter'}
+
+    ]
+
+
+    // autoGroupColumnDef: ColDef = {
+    //     headerName: 'Name',
+    //     field: 'name',
+    //     cellRenderer: 'agGroupCellRenderer',
+    //     cellRendererParams: {
+    //         checkbox: true
+    //     }
+    // };
+    rowData: Observable<any[]>;
+
+    constructor(private http: HttpClient) {
+        this.rowData = this.http.get<any[]>('http://localhost:8081/');
+    }
 }
